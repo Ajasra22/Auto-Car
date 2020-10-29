bd0c1cce6437e3f4c27b6e46b5f62d34158edc7c
Ajasra Gupta <60650011+Ajasra22@users.noreply.github.com>
2020-10-29 16:58:39 +0530

    Add files via upload

---
+++ Auto_Car/env/car_env.py
@@ -0,0 +1,18 @@
+
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class AutoCar(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self,close=False):
+        pass
